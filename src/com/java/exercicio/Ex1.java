3b1873aaaf3658be642b4e1d0786ec243ed7d2d7
DTI <silveiramel72@gmail.com>
2025-02-27 18:55:14 -0300

    aula

---
+++ src/com/java/exercicio/Ex1.java
@@ -0,0 +1,10 @@
+package com.java.exercicio;
+import java.util.Scanner;
+import java.util.Locale;
+
+public class Ex1 {
+    public static void main(String[] args) {
+        System.out.println("1");
+        System.out.printf("2\n");
+    }
+}
