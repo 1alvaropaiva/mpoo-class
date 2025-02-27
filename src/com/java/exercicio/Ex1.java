c4e00723f52581ab87b1aa2939c0ca898a733fcb
DTI <silveiramel72@gmail.com>
2025-02-27 19:18:39 -0300

    aula 27/02

--- src/com/java/exercicio/Ex1.java
+++ src/com/java/exercicio/Ex1.java
@@ -4,7 +4,17 @@ import java.util.Locale;
 
 public class Ex1 {
     public static void main(String[] args) {
-        System.out.println("1");
-        System.out.printf("2\n");
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Digite seu nome: ");
+        String nome = scan.next();
+        System.out.println("Digite seu peso: ");
+        double peso = scan.nextDouble();
+        System.out.println("Digite sua altura: ");
+        float altura = scan.nextFloat();
+        System.out.println(String.format("Nome: %s\nPeso: %.2f\nAltura: %.2f\n", nome, peso, altura));
+        System.out.printf("IMC: %.2f", peso/(altura*altura));
+
+        scan.close();
     }
 }
